7149206b4401d80453443748c9ff996872b9716b
Nathan Lopez <nathancarllopez@gmail.com>
2023-03-03 21:24:14 -0600

    Added Exponential class

--- main.py
+++ main.py
@@ -20,81 +20,6 @@ class DifferentiableFunction():
     def __truediv__(self, other):
         return DifferentiableFunction(lambda x: self.rule(x) / other.rule(x))
     
-class Monomial1(DifferentiableFunction):
-    def base_rule(x, coeff, power):
-        terms = (x for i in range(power))
-        return coeff * math.prod(terms)
-    
-    def __init__(self, coeff, power):
-        self.coeff = coeff
-        self.power = power
-        rule = lambda x: Monomial.base_rule(x, coeff, power)
-        DifferentiableFunction.__init__(self, rule)
-
-    def derivative(self):
-        return Monomial(self.coeff * self.power, self.power - 1)
-    
-    def __str__(self):
-        if self.power > 1:
-            if self.coeff == 1:
-                return 'x^' + str(self.power)
-            elif self.coeff == -1:
-                return '-x^' + str(self.power)
-            elif self.coeff == 0:
-                return str(self.coeff)
-            else:
-                return str(self.coeff) + 'x^' + str(self.power)
-        elif self.power == 1:
-            if self.coeff == 1:
-                return 'x'
-            elif self.coeff == -1:
-                return '-x'
-            elif self.coeff == 0:
-                return str(self.coeff)
-            else:
-                return str(self.coeff) + 'x'
-        else:
-            return str(self.coeff)
-    
-class Polynomial1(DifferentiableFunction):
-    def base_rule(x, coeffs):
-        top_power = len(coeffs)
-        terms = []
-        for i in range(top_power):
-            terms.append(Monomial.base_rule(x, coeffs[i], top_power - 1 - i))
-        return sum(terms)
-    
-    def __init__(self, coeffs):
-        self.coeffs = coeffs
-        rule = lambda x: Polynomial.base_rule(x, coeffs)
-        DifferentiableFunction.__init__(self, rule)
-        top_power = len(coeffs)
-        self.terms = []
-        for i in range(top_power):
-            self.terms.append(Monomial(coeffs[i], top_power - 1 - i))
-
-    def __str__(self):
-        terms_as_strings = list(map(str, self.terms))
-        first_term = ''
-        while first_term == '':
-            lead = terms_as_strings.pop(0)
-            if lead != '0':
-                first_term = lead
-        result = first_term
-        for i in range(len(terms_as_strings)):
-            next_term = terms_as_strings[i]
-            if next_term == '0':
-                continue
-            if next_term[0] == '-':
-                new_term = next_term[1:]
-                result += ' - ' + new_term
-            else:
-                result += ' + ' + next_term
-        return result
-    
-    def derivative(self):
-        pass
-
 class Polynomial(DifferentiableFunction):
     def base_rule(x, coeffs):
         num_of_terms = len(coeffs)
@@ -180,12 +105,19 @@ class Polynomial(DifferentiableFunction):
         deriv_coeffs.pop()
         return Polynomial(deriv_coeffs)
 
+class Exponential(DifferentiableFunction):
+    def __init__(self, coeff, base):
+        self.coeff = coeff
+        self.base = base
+        DifferentiableFunction.__init__(self, lambda x: coeff * math.pow(base, x))
 
-class Monomial(Polynomial):
-    pass
+    def __str__(self):
+        return str(self.coeff) + '*' + str(self.base) + '^x'
+    
+    def derivative(self):
+        return Exponential(round(self.coeff * math.log(self.base), 2), self.base)
 
-for j in range(5):
-    f = Polynomial([random.randint(-3,3) for k in range(7)])
-    print("function:", f)
-    print("derivative:", f.derivative())
-    print()
+f = Exponential(3,5)   
+print(f)
+print(f.derivative())
+print(math.log(5)*3)
