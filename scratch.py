4d061ca71a4b7e898f16e6229c31570f312deab4
Nathan Lopez <nathancarllopez@gmail.com>
2023-03-08 12:12:03 -0600

    Added new test to scratch
    This test reveals that there is a recursion issue
    with the get_subtrees_below_root method

--- scratch.py
+++ scratch.py
@@ -5,3 +5,9 @@ g = basicfunctions.Polynomial('x', [2,0,3,-1])
 h = f+g
 hTree = h.structure
 print(hTree)
+k = h+h
+print()
+kTree = k.structure
+print(kTree)
+print()
+print(kTree.get_subtrees_below_root())
